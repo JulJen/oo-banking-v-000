e1d26bbdd33224a1eeb2af63e50579df181ad536
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-22 18:12:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
+
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = 'open'
+    end
   # your code here
 end
