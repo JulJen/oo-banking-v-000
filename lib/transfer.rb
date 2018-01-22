62b44bc0657fb741f7bf3405cd929e2a0b29bc87
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-22 18:15:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
 
-    attr_accessor :name, :balance, :status
-    # attr_reader :name
+    attr_accessor :amount, :status
+    attr_reader :name
 
     def initialize(name)
       @name = name
-      @balance = 50
+      @amount = 50
       @status = 'pending'
     end
   # your code here
